c7306e58e94ad56d43bd8383f1ef8da3df5f9386
marvynlevin <gamerkyrhoz@gmail.com>
2025-05-27 16:13:22 +0200

    Third commit

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,44 +1,43 @@
-import forms from '@tailwindcss/forms'
-import typography from '@tailwindcss/typography'
+const forms = require('@tailwindcss/forms')
+const typography = require('@tailwindcss/typography')
 
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-        './index.html',
-        './src/**/*.{html,vue,js,ts,jsx,tsx,css}'
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                playfair: ['"Playfair Display"', 'serif'],
-            },
-            colors: {
-                espresso: '#3C2821',
-                mocha: '#583C0E',
-                blush: '#BEAAAA',
-                dusk: '#8D7474',
-                linen: '#F3E8D8',
-                vanilla: '#FFF3E2',
-            },
-            fontWeight: {
-                black: '900',
-            },
-            fontSize: {
-                '120px': '120px',
-                '50px': '50px',
-                '35px': '35px',
-                '30px': '30px',
-            },
-            keyframes: {
-                floatY: {
-                    '0%, 100%': { transform: 'translateY(0)' },
-                    '50%': { transform: 'translateY(40px)' }
-                }
-            },
-            animation: {
-                floatY: 'floatY 3s ease-in-out infinite'
-            }
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{html,vue,js,ts,jsx,tsx,css}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        playfair: ['"Playfair Display"', 'serif'],
+      },
+      colors: {
+        espresso: '#3C2821',
+        mocha: '#583C0E',
+        blush: '#BEAAAA',
+        dusk: '#8D7474',
+        linen: '#F3E8D8',
+        vanilla: '#FFF3E2',
+      },
+      fontWeight: {
+        black: '900',
+      },
+      fontSize: {
+        '120px': '120px',
+        '50px': '50px',
+        '35px': '35px',
+        '30px': '30px',
+      },
+      keyframes: {
+        floatY: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(40px)' }
         }
-    },
-    plugins: [forms, typography],
-};
+      },
+      animation: {
+        floatY: 'floatY 3s ease-in-out infinite'
+      }
+    }
+  },
+  plugins: [forms, typography],
+}
